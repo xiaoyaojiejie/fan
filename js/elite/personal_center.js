edfab9fda6eeb18f86175c80f4dd76bb25ddb225
lanou <lan@lanoudeMac-mini-54.local>
2016-07-23 09:23:13 +0800

    123

---
+++ js/elite/personal_center.js
@@ -0,0 +1,12 @@
+$(function(){
+	for (var i = 0;i < $("#menu li").length;i++) {
+		$("#menu li").on("click",function(){
+			$(this).addClass("menu_active").siblings().removeClass("menu_active");
+			$(".main_body>div").eq($(this).index()).show().siblings().hide()
+		})
+	}
+	
+	for (var i = 0; i < $(".learning_curve_bottom>.course_btn_box>a").length) {
+		
+	}
+})
