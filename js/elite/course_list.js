c27309ad702782f373e05745ba25338aaaf8012e
lanou <lan@lanoudeMac-mini-54.local>
2016-07-27 15:54:41 +0800

    11

---
+++ js/elite/course_list.js
@@ -0,0 +1,4 @@
+window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdPic":"","bdStyle":"0","bdSize":"16"},"share":{},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+$("#share").on("click",function(){
+	$(".bdsharebuttonbox").toggle();
+})
