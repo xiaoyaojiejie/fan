1e6d7238bd1d3c60be6c021685bb67fd9dd412fc
lanou <lan@lanoudeMac-mini-54.local>
2016-08-01 09:28:28 +0800

    8-1

--- js/elite/course_list.js
+++ js/elite/course_list.js
@@ -1,13 +1,17 @@
 window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdPic":"","bdStyle":"0","bdSize":"16"},"share":{},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
 $("#share").on("click",function(){
 	$(".bdsharebuttonbox").toggle();
+	return false;
 })
-
-	$(".subject_details>ul>li>div").on("mouseenter",function(){
-		$(this).children("span").hide();
-		$(this).children("a").show();
-	})
-	$(".subject_details>ul>li>div").on("mouseleave",function(){
-		$(this).children("span").show();
-		$(this).children("a").hide();
-	})
+$(document).on("click",function(e){
+	$(".bdsharebuttonbox").hide();
+	
+})
+$(".subject_details>ul>li>div").on("mouseenter",function(){
+	$(this).children("span").hide();
+	$(this).children("a").show();
+})
+$(".subject_details>ul>li>div").on("mouseleave",function(){
+	$(this).children("span").show();
+	$(this).children("a").hide();
+})
